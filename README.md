3573663a31de4b74f4a29c9e82764bc3eee294e9
VicSz <vicsz>
2019-10-16 09:56:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+= Spring Cloud Gateway + Resource Server + Webflux , multiple instance test on PCF
+
+== Run the Sample
+
+* Build and Deploy the Gateway
+
+** From the gateway directory :
+
+```sh
+gradle build
+cf push my-gateway -i 2 -p build/libs/gateway-1.0-SNAPSHOT.jar
+cf set-env my-gateway uaa-url MY-UAA-URL
+cf set-env my-gateway uaa-client-id MY-UAA-CLIENT-ID
+cf set-env my-gateway uaa-client-secret MY-UAA-CLIENT-SECRET
+cf vic-gateway restage
+```
+
+> Note: replace MY-UAA-URL with the URL for your UAA Provider for example : https://myuaa.login.run.pivotal.io
+> Note: replace MY-UAA-CLIENT-ID and MY-UAA-CLIENT-SECRET with your generated UAA Client ID and Secret
+> Note: in the application.yml update *resource-server-url* and *redirect-uri-template* if needed
+
+* Build and Deploy the Resource Server
+
+** From the resource server directory :
+
+```sh
+gradle build
+cf push vic-resource-server -i 2 -p build/libs/resource-server-1.0-SNAPSHOT.jar 
+cf set-env vic-resource-server uaa-url MY-UAA-URL
+cf vic-gateway restage
+```
---
+++ gateway/src/main/java/sample/GatewayApplication.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class GatewayApplication {
+
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
+
+	@Value("${application.resource-server-url}")
+	private String resourceServerUrl;
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		//@formatter:off
+		return builder.routes()
+				.route("resource", r -> r.path("/resource")
+						.filters(f -> f.filter(filterFactory.apply()))
+						.uri(resourceServerUrl))
+				.build();
+		//@formatter:on
+	}
+
+	@GetMapping("/")
+	public String index(Model model,
+						@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+						@AuthenticationPrincipal OAuth2User oauth2User) {
+		model.addAttribute("userName", oauth2User.getName());
+		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+		model.addAttribute("userAttributes", oauth2User.getAttributes());
+		return "index";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
